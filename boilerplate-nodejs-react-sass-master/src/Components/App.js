2aa2725584b1a41df326c5b216d5cf9692b1bd19
Alejandro Ortega <43015395+AlejoOrtega@users.noreply.github.com>
2019-03-05 14:15:13 -0500

    Add files via upload

---
+++ boilerplate-nodejs-react-sass-master/src/Components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+    
+	render() {
+    	return (
+    	  <main>
+
+				<p>hahaha</p>
+
+    	  </main>
+    	)
+	}
+}
+export default App
